54db11c14603986c773878930fb6c45c88e3298a
Pedro Graça <74125655+zalcobia@users.noreply.github.com>
2021-11-16 10:44:03 +0000

    Update google-fonts.sh

--- google-fonts.sh
+++ google-fonts.sh
@@ -4,12 +4,13 @@
 # Based on AUR package <https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ttf-google-fonts-git>
 
 # dependancies: fonts-cantarell, ttf-ubuntu-font-family, ttf-mscorefonts-installer, git
-sudo apt-get install fonts-cantarell ttf-ubuntu-font-family ttf-mscorefonts-installer git
+sudo apt install fonts-cantarell ttf-ubuntu-font-family ttf-mscorefonts-installer git
 srcdir="/tmp/google-fonts"
 pkgdir="/usr/share/fonts/truetype/google-fonts"
 giturl="git://github.com/google/fonts.git"
 giturl="git://github.com/adobe-fonts/source-code-pro.git"
 giturl="git://github.com/source-foundry/Hack.git"
+giturl="git://github.com/googlefonts/roboto.git"
 
 mkdir $srcdir
 cd $srcdir
@@ -23,9 +24,6 @@ sudo find $srcdir -type f -name "*.ttf" -exec install -Dm644 {} $pkgdir \;
 echo "Cleaning up..."
 sudo find $pkgdir -type f -name "Cantarell-*.ttf" -delete \;
 sudo find $pkgdir -type f -name "Ubuntu-*.ttf" -delete \;
-
-# provides roboto
-sudo apt-get --purge remove fonts-roboto
 
 echo "Updating font-cache..."
 sudo fc-cache -f > /dev/null
